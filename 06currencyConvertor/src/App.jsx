8a90dcb20e156b6b819b92f24ff2c82879f538cd
Sujal <sujalkesharwani220@gmail.com>
2024-02-04 23:00:35 +0530

    new project started

---
+++ 06currencyConvertor/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="uppercase text-3xl text-white">Currency convertor</h1>
+    </>
+  );
+}
+
+export default App;
